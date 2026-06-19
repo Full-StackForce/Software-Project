4afd8cdceb8fc192014351bc69a2abd1263bb978
dspatel0628 <dspatel0628@gmail.com>
2026-06-19 12:33:08 -0400

    Created frontend folder and added frontend for login page

--- main.py
+++ main.py
@@ -5,8 +5,18 @@ from routers import index as indexRoute
 from dependencies.config import conf
 from routers import auth
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 print("AUTH ROUTES:")
