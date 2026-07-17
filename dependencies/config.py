ee4d922600bb32de4fb49a94d2ba5d401d19af23
dspatel0628 <dspatel0628@gmail.com>
2026-07-17 16:49:44 -0400

    Use environment variables for database configuration

--- dependencies/config.py
+++ dependencies/config.py
@@ -1,9 +1,9 @@
 ##Connect to sql database
 class conf:
-    db_host = "YOUR_HOST"
+    db_host = "localhost"
     db_name = "pulsepoint"
     db_port = 3306
     db_user = "root"
-    db_password = "PASSWORD"
+    db_password = "NewStrongPassword123!"
     app_host = "localhost"
     app_port = 8000
