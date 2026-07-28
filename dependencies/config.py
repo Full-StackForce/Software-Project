213954d1485922e122e66e912cf689d94c423b64
Jennifer Vazquez <jvazqu20@charlotte.edu>
2026-07-28 14:14:36 -0400

    Update login page

--- dependencies/config.py
+++ dependencies/config.py
@@ -4,6 +4,6 @@ class conf:
     db_name = "pulsepoint"
     db_port = 3306
     db_user = "root"
-    db_password = "NewStrongPassword123!"
+    db_password = "PASSWORD"
     app_host = "localhost"
     app_port = 8000
