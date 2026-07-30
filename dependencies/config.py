f80bec76ee0f959d543c066db9597218678ad9a5
Jennifer Vazquez <jvazqu20@charlotte.edu>
2026-07-30 00:21:56 -0400

    Update dependcies for deployment.

--- dependencies/config.py
+++ dependencies/config.py
@@ -1,9 +1,9 @@
 ##Connect to sql database
+import os
+
 class conf:
-    db_host = "YOURHOST"
-    db_name = "pulsepoint"
-    db_port = 3306
-    db_user = "root"
-    db_password = "PASSWORD"
-    app_host = "localhost"
-    app_port = 8000
+    db_host = os.getenv("DB_HOST", "localhost")
+    db_port = int(os.getenv("DB_PORT", "3306"))
+    db_user = os.getenv("DB_USER", "root")
+    db_password = os.getenv("DB_PASSWORD", "")
+    db_name = os.getenv("DB_NAME", "pulsepoint")
